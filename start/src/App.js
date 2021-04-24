c9a58c473608f9a36ed4df9ea3408202e35e0176
Vidhi Mittal <vmittal5@hawk.iit.edu>
2021-04-23 20:22:56 -0500

    initial

---
+++ start/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Login from "./components/Login"
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
